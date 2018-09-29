e6fac23b1eec3d9e2d19ad2ca5e9158fc8306c48
wanmudong <1903172774@qq.com>
2018-09-29 22:25:02 +0800

    在个人设置界面显示已有信息

--- src/main/java/com/oa/me/Dao/CampusDao.java
+++ src/main/java/com/oa/me/Dao/CampusDao.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Select;
  * Created by chenjiehao on 2018/9/22
  */
 @Mapper
-public interface CampusDao {
+public interface    CampusDao {
 
     @Select("select dict_item_name from oa_dict_campus where dict_id = #{campus}")
     String getCampusName(@Param("campus") int campus);
